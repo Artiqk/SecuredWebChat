b93a6646366850266f4f1fe7d1f41def31e8e572
Antoine Abonneau <antoineabonneau94@gmail.com>
2024-06-01 05:07:49 +0200

    removed createUser => useless

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,6 @@
 const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
-const saltRounds = 10;
-
-exports.createUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-
-    const user = await User.create({
-      username: username,
-      password: hashedPassword
-    });
-
-    // FIXME: Password returned in data
-    res.status(201).json({ message: 'User created', data: user });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll({
