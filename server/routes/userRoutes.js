d054a15f6d8eeb79b391c19111978f22f802fe1e
Antoine Abonneau <antoineabonneau94@gmail.com>
2024-06-01 17:16:09 +0200

    fix v2

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { createUser, getAllUsers, getUserByUsername, getUserById } = require('../controllers/userController');
 
-router.post('/new', createUser);
 router.get('/fetch', getAllUsers);
 router.get('/fetch/:username', getUserByUsername);
 router.get('/fetch/:userId', getUserById);
